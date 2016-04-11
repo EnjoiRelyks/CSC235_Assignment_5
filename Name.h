b20379836a6994f34ca843eaede51bfa76f33fcd
ebenson1 <ebenson1@murraystate.edu>
2016-04-11 11:19:36 -0500

    Create Name.h

---
+++ Name.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+using namespace std;
+
+struct Name
+{
+	string fName;
+	string lName;
+	char mInitial;
+};
+
+Name getName(string firstName, string lastName, char middleInitial);
+
+string getString(Name fullName);
