96cbadfef4dff9ec08b0e65218494e6117a02c2d
EnjoiRelyks <cooksterlisa2007@gmail.com>
2016-04-12 11:25:11 -0500

    Update main.cpp
    • Went through main.cpp and reduced the tab spacing to <4> as opposed to 8. 
    • Provided some comments for various sections (not that Tennyson needs them but for general practice).

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 /*
-AUTHOR:		Skyler Reeves & Ethan Benson
+AUTHOR(S):	Skyler Reeves & Ethan Benson
 DATE:		04-14-2016
 TITLE:		Assignment 5, CSC 235
 PURPOSE:	First, the program will prompt the user to input triangle side lengths and then 
@@ -17,19 +17,21 @@ using namespace std;
 
 int main()
 {
+	//Variable Declaration and initialization through user input. 
 	unsigned int a, b, c;
 	cout << "Please enter 3 integers each separated by a space and press <ENTER>: ";
 	cin >> a >> b >> c;
 
+	//Calls to the getTriangle and getString functions.
 	Triangle triangle1 = getTriangle(a, b, c);
 	cout << getString(triangle1) << endl;
 
-	//**********************************************************************
-
+	//Variable Declarations
 	string firstName;
 	string lastName;
 	char midInitial;
 
+	//Prompts the user for input (to pass into the getName & getString functions)
 	cout << endl << "Please enter your first name and press <ENTER>: ";
 	cin >> firstName;
 
@@ -39,8 +41,8 @@ int main()
 	cout << endl << "Please enter your last name and press <ENTER>: ";
 	cin >> lastName;
 
+	//Calls to the getName and getString functions
 	Name newName = getName(firstName, lastName, midInitial);
-
 	cout << endl << getString(newName) << endl;
 
 	return 0;
